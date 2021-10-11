13a1377e6ab1edb0fee76bd5dea284b91f6c0976
avan-pra <velovo.vanpraet@gmail.com>
2021-10-11 20:26:19 +0200

    initial front commit

---
+++ front/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul v-for="user in users" :key="user.id">
+      <li>{{ user.logName }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const users = await fetch(
+      'http://localhost:8000/api/users'
+    ).then((res) => res.json())
+
+    console.log(users)
+    return { users }
+  }
+}
+</script>
