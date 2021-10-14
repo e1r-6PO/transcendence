ad6f51982a6c4b2c1d5410b1554fe91d51c4a49e
Lucas MOULIN <lmoulin@e1r7p20.42.fr>
2021-10-14 17:54:50 +0200

    add auth page

---
+++ front/pages/auth/google/callback.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ ouaish.msg }}</h1>
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    async asyncData() {
+
+    var url
+
+    url = window.location.origin
+    url += '/api/auth/google/callback'
+    url += window.location.search
+
+    const ouaish = await axios.get(url).then(response => response.data)
+
+    console.log(ouaish)
+
+    return { ouaish }
+  }
+}
+</script>
