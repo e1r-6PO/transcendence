0b6b8e1854e85e23992da1d5b2c2849258debf8f
avan-pra <velovo.vanpraet@gmail.com>
2021-10-16 16:39:01 +0200

    fix me page

--- front/pages/me.vue
+++ front/pages/me.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>You are connected as: {{ me.email }}</h1>
-    <h2>Nickname: {{ me.nickname }}</h2>
+    <h2>Nickname: {{ me.nickName }}</h2>
     <!-- <p>Path: {{ $route.path }}</p> -->
     <NuxtLink to="/">Back to Home page</NuxtLink>
   </div>
@@ -9,10 +9,12 @@
 
 <script>
 export default {
-  async mounted() {
+  async asyncData() {
     const me = await fetch(
       '/api/users/me'
     ).then((res) => res.json())
+
+    return { me }
   }
 }
 </script>
