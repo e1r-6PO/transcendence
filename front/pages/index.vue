4eeba164a81758b19b5c3e2cef02bff6b5e2f0f4
EudaldV98 <janeudald98@gmail.com>
2021-10-20 18:42:14 +0200

    First page starting to look good

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,33 +2,36 @@
   <v-app dark>
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8" md="6">
-        <v-card-text 
-          class="text-center main_title_test"
-        >
+        <v-card-text class="text-center main_title_test">
           Welcome to Transcendence
         </v-card-text>
-        <v-card>
-          <v-card class="logo py-4 d-flex justify-center">
-            <hr class="my-3">
-            <a href="/api/auth/42">
-              <img
-              src="../assets/42_Logo.png" 
-              width="150"
-              height="150"
-              />
-              <!-- Nuxt Documßntation -->
-            </a>
-            <br>
-            <a href="/api/auth/google">
-              <img
-                src="../assets/Google_Button.png" 
-                width="200"
-                height="150"
-              />
-              <!-- Nuxt GitHub -->
-            </a>
-          </v-card>
-        </v-card>
+        <v-main>
+          <v-btn
+            dark
+            width="110"
+            height="110"
+            href="/api/auth/42"
+            style="float: left;"
+          >
+            <icon-42 />
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            width="110"
+            height="110"
+            href="/api/auth/google"
+            style="float: right;"
+          >
+            <v-icon
+              width="100"
+              height="100"
+              large
+            >
+            mdi-google
+            </v-icon>
+          </v-btn>
+        </v-main>
       </v-col>
     </v-row>
   </v-app>
