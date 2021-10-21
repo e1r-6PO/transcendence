3cc1218ba3ddbccd57a57191a498338d47ac54c3
avan-pra <velovo.vanpraet@gmail.com>
2021-10-21 15:45:11 +0200

    modify front provider

--- front/pages/auth/_provider/callback.vue
+++ front/pages/auth/_provider/callback.vue
@@ -1,22 +1,22 @@
 <template>
 </template>
 <script>
-import axios from "axios";
-
 export default {
     async mounted() {
+      const { params: { provider } } = this.$route
+0
 
     var url
 
     url = window.location.origin
-    url += '/api/auth/42/callback'
+    url += '/api/auth/'
+    url += provider
+    url += '/callback'
     url += window.location.search
 
-    console.log(window.location.search)
+    console.log(url)
 
-    const ret = await axios.get(url).then(response => response.data)
-
-    console.log(ret)
+    const ret = await this.$axios.$get(url)
 
     window.location.href = '/me'
   }
