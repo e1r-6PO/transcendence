f0598e591bf41fe4ab170548c493b5e73739a5aa
lucasmln <lmoulin@e1r6p20.42.fr>
2021-10-28 19:49:30 +0200

    add set_nickname page

---
+++ front/pages/profile/set_nickname.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-main>
+    <v-text-field
+      class="foreground_element"
+      v-model="login.nickname"
+      label="Nickname"
+      counter="20"
+      shaped
+      filled
+      :rules="rules.nickname"
+    >
+    </v-text-field>
+    <v-btn 
+      class="foreground_element"
+      text
+      :disabled="!nickIsValid"
+      @click="setNick"
+    >
+      Complet Registration
+    </v-btn>
+  </v-main>
+</template>
+
+<script>
+
+
+export default {
+
+  async asyncData({ $axios }) {
+    const ret = await $axios.$get('api/users/me/nickname')
+
+    if (ret.nickname != "")
+      window.location.href = '/me'
+  },
+  data() {
+
+    const defaultLogin = Object.freeze({
+        nickname: ''
+    })
+
+    return {
+      login: Object.assign({}, defaultLogin),
+      rules: {
+        nickname: [val => (val || '').length > 0 || 'This field is required'],
+      }
+    }
+  },
+
+  computed: {
+    nickIsValid() {
+      return this.login.nickname &&
+      this.login.nickname.length <= 20
+    },
+  },
+
+  methods: {
+    async setNick() {
+        const ret = await this.$axios.post('api/users/me/nickname?nickname=' + this.login.nickname)
+          .catch(function (error) {
+            return error.response
+        });
+        if (ret.status == 201)
+          window.location.href = '/me'        
+      },
+  }
+}
+</script>
