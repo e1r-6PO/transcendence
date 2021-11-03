4b7734f6976c94fc1322d7e7eaaf139e2f565602
avan-pra <velovo.vanpraet@gmail.com>
2021-11-03 12:56:30 +0100

    add exception filter calss (not enabled)

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,10 +2,12 @@ require('https').globalAgent.options.rejectUnauthorized = false;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 import * as cookieParser from 'cookie-parser';
+// import { AllExceptionFilter } from './exception/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+  // app.useGlobalFilters(new AllExceptionFilter())
   await app.listen(3000);
 }
 bootstrap();
