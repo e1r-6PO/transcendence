6f3d5ea1406afea9bd500e8fc6e84e03e5bd237d
avan-pra <velovo.vanpraet@gmail.com>
2021-11-03 15:54:22 +0100

    popup if nick is already owned

--- front/pages/profile/set_nickname.vue
+++ front/pages/profile/set_nickname.vue
@@ -55,8 +55,10 @@ export default {
 
   methods: {
     async setNick() {
-        const ret = await this.$axios.post('api/users/me/nickname?nickname=' + this.login.nickname)
+        const nick = this.login.nickname
+        const ret = await this.$axios.post('api/users/me/nickname?nickname=' + nick)
           .catch(function (error) {
+            alert("nick " + nick + " is already taken")
             return error.response
         });
         if (ret.status == 201)
