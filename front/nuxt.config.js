3ccdab578ec7e224543d50e477e67b7ba4f79cdd
EudaldV98 <janeudald98@gmail.com>
2021-11-11 20:34:26 +0100

    reset default config

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -79,30 +79,16 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      options: {
-        customProperties: true,
-      },
-      dark: true,
+      dark: false,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-          background: colors.indigo.accent2
-        },
-        light: {
-          background:"#ff0000",
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          primary: '#f28482',
+          accent: '#f28482',
+          secondary: '#f28482',
+          info: '#f28482',
+          warning: '#f28482',
+          error: '#f28482',
+          success: '#f28482'
         }
       }
     }
