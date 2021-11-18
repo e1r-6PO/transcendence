51835b5d10684e35cbd4411d559536ad4e0101e7
Jan eudald VAQUER <jvaquer@e1r5p1.42.fr>
2021-11-18 18:45:12 +0100

    fix in back for socket

--- back/src/service/message.service.ts
+++ back/src/service/message.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { Message } from '../entity/message.entity';
+// import { Injectable } from '@nestjs/common';
+// import { Message } from '../entity/message.entity';
 
-@Injectable()
-export class MessageService {
+// @Injectable()
+// export class MessageService {
 
-    public getAll = async (): Promise<Message[]> => {
-        return Message.find();
-    }
+//     public getAll = async (): Promise<Message[]> => {
+//         return Message.find();
+//     }
 
-    public createMessage = async (sender: string, message: string): Promise<Message> => {
-        const newMsg: Message = new Message(sender, message);
-        return await newMsg.save();
-    }
+//     public createMessage = async (sender: string, message: string): Promise<Message> => {
+//         const newMsg: Message = new Message(sender, message);
+//         return await newMsg.save();
+//     }
 
-}
+// }
