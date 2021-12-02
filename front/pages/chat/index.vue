017d5c198bf26418301bcc6b1c3a8a67ae5fb5f6
lucasmln <lucas.moulin78114@gmail.com>
2021-12-02 12:08:31 +0100

    add sender nick in bubble msg

--- front/pages/chat/index.vue
+++ front/pages/chat/index.vue
@@ -1,93 +1,132 @@
 <template>
-  <v-container align-items="center" style="margin-top: 3%">
-    <div style="overflow-y: scroll; max-height: 900px">
-      <!-- <v-virtual-scroll class="foreground_element" itemHeight="30" height="100" items="messagesArray"> -->
-          <v-list-item
-            v-for="(msg, i) in messagesArray"
-            :key="i"
-            style="margin-top: 15px;"
-          >
-        <v-card class="text-left bubble" style="min-width: 70px; max-width: 400px !important">
-            <v-list-item-content style="margin-left: 10px; margin-right: 10px">
-              <v-list-item-title v-text="msg.message"></v-list-item-title> 
-              <v-list-item-subtitle class="text-right" v-text="formateTime(msg.time)"></v-list-item-subtitle>
-            </v-list-item-content>
+  <v-container
+    align-items="center"
+    style="margin-top: 3%;"
+    class="body overflow-y-hidden"
+    
+  >
+    <div class="overflow-y-auto">
+      <v-list
+        style="max-height: 800px !important; height: 800px"
+        class="overflow-y-auto"
+        color="#181818"
+      >
+      <v-list-item
+        v-for="(msg, i) in messagesArray"
+        :key="i"
+        style="margin-top: 15px;"
+      >
+        <v-card
+          class="text-left bubble"
+          style="min-width: 70px; max-width: 400px !important"
+        >
+          <v-list-item-content style="margin-left: 10px; margin-right: 10px">
+            <v-list-item-subtitle
+              class="text-left"
+              v-text="formatNick(msg)"
+            ></v-list-item-subtitle>
+            <v-list-item-title v-text="msg.message"></v-list-item-title>
+            <v-list-item-subtitle
+              class="text-right"
+              v-text="formateTime(msg.time)"
+            ></v-list-item-subtitle>
+          </v-list-item-content>
         </v-card>
-        </v-list-item>
+      </v-list-item>
+      </v-list>
       <!-- </v-virtual-scroll> -->
     </div>
 
-    <v-text-field
-      style="margin-top: 3%"
-      background-color="white"
-      color="blue"
-      v-model="message"
-      filled
-      clear-icon="mdi-close-circle"
-      clearable
-      label="Message"
-      elevation="2"
-      @keypress.enter="sendMessage"
-    >
-      <v-icon slot="append-outer" color="blue"> mdi-send </v-icon>
-    </v-text-field>
+    <v-footer app inset color="#181818">
+      <v-text-field
+        style="margin-top: 3%"
+        background-color="white"
+        color="blue"
+        v-model="message"
+        filled
+        clear-icon="mdi-close-circle"
+        clearable
+        label="Message"
+        elevation="2"
+        @keypress.enter="sendMessage"
+      >
+        <v-icon slot="append-outer" color="blue"> mdi-send </v-icon>
+      </v-text-field>
+    </v-footer>
   </v-container>
 </template>
 
 <script lang='ts'>
-
 import Vue from 'vue'
-import { io } from 'socket.io-client';
-import VueSocketIOExt from 'vue-socket.io-extended';
+import { io } from 'socket.io-client'
+import VueSocketIOExt from 'vue-socket.io-extended'
 import { Messages } from '../../assets/Messages'
+import { LightUser } from '../../assets/User'
 
 const socket = io('http://localhost:3000', {
-    withCredentials: true
+  withCredentials: true,
 })
 Vue.use(VueSocketIOExt, socket)
 
 export default Vue.extend({
-
   middleware: 'login',
 
   data() {
     return {
       message: '',
       messagesArray: new Array<Messages>(),
+      usersNick: new Map()
     }
   },
 
   methods: {
     sendMessage(): void {
-      this.$socket.client.emit('msgToServer', this.message);
-        this.message = '';
-      },
-    
-    formateTime(time: Date): string {
+      this.$socket.client.emit('msgToServer', this.message)
+      this.message = ''
+    },
 
-      var newTime : Date = new Date(time)
-      return newTime.getHours() + ":" + newTime.getMinutes()
+    formateTime(time: Date): string {
+      var newTime: Date = new Date(time)
+      return newTime.getHours() + ':' + (newTime.getMinutes() < 10 ? '0' + newTime.getMinutes() : newTime.getMinutes())
+    },
+
+    formatNick(nick: Messages): string {
+      console.log("nick in subtitle : " + nick.senderNick)
+      return nick.senderNick
     }
   },
 
   async mounted() {
     this.messagesArray = await this.$axios.$get('/api/chat/messages')
+
+    // for (var i = 0; i < this.messagesArray.length; i++)
+    // {
+    //   var userId = this.messagesArray[i].senderId
+    //   var fullMsg: Messages = new Messages(this.messagesArray[i])
+    //   if (this.usersNick.get(userId) == undefined)
+    //   {
+    //     var user: LightUser;
+    //     user = await this.$axios.$get('/api/users?id=' + userId)
+    //     if (user)
+    //       this.usersNick.set(userId, user.nickName)
+    //   }
+    //   fullMsg.senderNick = this.usersNick.get(userId)
+    //   this.messagesArray[i] = new Messages(fullMsg)
+    // }
+    console.log("test nick msg " + this.messagesArray[0].senderNick)
     console.log(this.messagesArray)
     this.$socket.$subscribe('msgToClient', (msg: Messages) => {
-      this.messagesArray.push(msg);
-      console.log(this.$socket);
+      this.messagesArray.push(msg)
     })
-  }
+  },
 })
-
 </script>
 
 <style scoped>
-
 .flex-container {
   /* We first create a flex layout context */
   display: flex;
-  
+
   /* Then we define the flow direction 
      and if we allow the items to wrap 
    * Remember this is the same as:
@@ -95,7 +134,7 @@ export default Vue.extend({
    * flex-wrap: wrap;
    */
   flex-flow: column wrap;
-  
+
   /* Then we define how is distributed the remaining space */
   justify-content: center;
   align-content: center;
@@ -109,7 +148,8 @@ export default Vue.extend({
   color: #000; /* bubble text color */
 }
 
-.bubble:before, .bubble:after {
+.bubble:before,
+.bubble:after {
   border-radius: 200px / 5px;
   content: '';
   display: block;
@@ -121,6 +161,11 @@ export default Vue.extend({
   border-bottom-color: #ffffff !important; /* arrow color */
   bottom: 0px !important;
   left: -7px !important;
+}
+
+body {
+  overscroll-behavior: none;
+  overflow-y: hidden;
 }
 
 </style>
