7f3ae5b7e5602accc784fcfc1e7f2cbc2ea53560
lucasmln <lucas.moulin78114@gmail.com>
2021-12-02 12:07:11 +0100

    add copy constructor for msg and add senderNick var

--- front/assets/Messages.ts
+++ front/assets/Messages.ts
@@ -1,16 +1,28 @@
+interface IMessages {
+    id: number;
+    senderId: number;
+    senderNick: string;
+    message: string;
+    time: Date;
+}
+
 class Messages {
 
     id: number;
     senderId: number;
+    senderNick: string;
     message: string;
     time: Date;
 
-    constructor()
+    constructor();
+    constructor(msg: IMessages);
+    constructor(msg?: any)
     {
-        this.id = 0;
-        this.senderId = 0;
-        this.message = "";
-        this.time = new Date();
+        this.id = msg && msg.id || 0;
+        this.senderId = msg && msg.senderId || 0;
+        this.senderNick = msg && msg.senderNick || "";
+        this.message = msg && msg.message || "";
+        this.time = msg && msg.time || new Date();
     }
 }
 
