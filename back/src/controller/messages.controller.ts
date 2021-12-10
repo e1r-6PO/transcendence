e6cd76fd8a33e31e793f6c38c8a7dfe10c883e48
lmoulin <lucas.moulin78114@gmail.com>
2021-12-10 19:06:01 +0100

    je sais plus quoi commit

--- back/src/controller/messages.controller.ts
+++ back/src/controller/messages.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query, Res, HttpCode, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query, Res, HttpCode, UseInterceptors, UploadedFile, UseGuards, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Request, Response } from 'express'
-import { Relationship } from 'src/entity/relationship.entity';
+import { query, Request, Response } from 'express'
 import { HasNickGuard, TwoFaGuard, ValidTokenGuard } from 'src/guards/account.guards';
-import { UsersService } from 'src/service/users.service';
 import { Repository } from 'typeorm';
 import { Messages } from 'src/entity/messages.entity'
 import { MessagesService } from 'src/service/messages.service';
@@ -14,7 +12,7 @@ export class MessagesController {
   constructor(
     private readonly messagesService: MessagesService,
     @InjectRepository(Messages)
-    private readonly MessagesRepository : Repository<Messages>
+    private readonly MessagesRepository : Repository<Messages>,
   ) {}
 
   @Get('messages')
