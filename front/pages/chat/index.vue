b50dff29084c135d87e1890f50883010d3b82661
lmoulin <lucas.moulin78114@gmail.com>
2021-12-10 19:01:02 +0100

    add join channel button

--- front/pages/chat/index.vue
+++ front/pages/chat/index.vue
@@ -59,6 +59,52 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog
+      v-model="dialogJoin"
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Join channel
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Channel settings</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-text-field
+              label="Channel name"
+              v-model="channName"
+            ></v-text-field>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialogJoin = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="joinChannel()"
+            :disabled="disableJoin()"
+          >
+            Join
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </v-container>
 </template>
@@ -74,7 +120,9 @@ export default Vue.extend({
   data() {
     return {
       dialog: false,
+      dialogJoin: false,
       channName: '',
+      channPass: '',
       channType: '',
       typeList: [
         'Public',
@@ -89,13 +137,25 @@ export default Vue.extend({
       console.log("Chan name: " + this.channName)
       console.log("Chan type: " + this.channType)
       this.dialog = false
+      this.$axios.$post('/api/chat/create?name=' + this.channName + '&type=' + this.channType)
       this.$router.push("/chat/" + this.channName)
+    },
+
+    joinChannel() {
+      this.dialogJoin = false;
+      this.$axios.$post('/api/chat/join?name=' + this.channName)
     },
 
     disableCreate() {
       if (this.channName == '' || this.channName.length > 20)
         return true
       if (this.channType == '')
+        return true
+      return false
+    },
+
+    disableJoin() {
+      if (this.channName == '')
         return true
       return false
     }
