df5a2230eb1826690466987953964baa281fedcd
lmoulin <lucas.moulin78114@gmail.com>
2021-12-17 17:50:03 +0100

    add class channelUser

---
+++ back/src/entity/channelUser.entity.ts
@@ -0,0 +1,27 @@
+import { Entity } from "typeorm";
+import { ChannelStatus } from "./channelParticipant.entity";
+
+@Entity()
+export class ChannelUser {
+  
+  constructor() {
+    this.id = 0;
+    this.nickName = '';
+    this.isMute = false;
+    this.muteTime = null;
+    this.channelStatus = ChannelStatus.default
+    this.picture = ''
+  }
+
+  id: number;
+
+  nickName: string;
+
+  channelStatus: string;
+
+  isMute: boolean;
+
+  muteTime: Date;
+
+  picture: string;
+}
