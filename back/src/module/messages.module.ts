e605f3202cf1ee6609f3cb1f0f21c573327155b5
lmoulin <lucas.moulin78114@gmail.com>
2021-12-17 04:18:32 +0100

    remove get messages from message module

--- back/src/module/messages.module.ts
+++ back/src/module/messages.module.ts
@@ -3,16 +3,17 @@ import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { MessagesController } from "src/controller/messages.controller";
 import { Channel } from "src/entity/channel.entity";
+import { ChannelParticipant } from "src/entity/channelParticipant.entity";
 import { Messages } from "src/entity/messages.entity";
 import { Relationship } from "src/entity/relationship.entity";
 import { User } from "src/entity/user.entity";
-import { MessagesService } from "src/service/messages.service";
+import { ChannelService } from "src/service/channel.service";
 import { UsersService } from "src/service/users.service";
 import { CustomJwtModule } from "./custom.jwt.module";
 
 @Module({
-  imports: [ TypeOrmModule.forFeature([Messages]), TypeOrmModule.forFeature([User]), TypeOrmModule.forFeature([Channel]), CustomJwtModule ],
+  imports: [ TypeOrmModule.forFeature([Messages]), TypeOrmModule.forFeature([User]), TypeOrmModule.forFeature([Channel, ChannelParticipant]), CustomJwtModule ],
   controllers: [ MessagesController ],
-  providers: [ MessagesService ]
+  providers: [ ChannelService ]
 })
 export class MessagesModule {}
