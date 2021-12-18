297b30bb78e011ad95a0c0656b0f1dbab38c2b9d
lmoulin <lucas.moulin78114@gmail.com>
2021-12-18 22:13:42 +0100

    design channel pepito dingo

--- front/pages/chat/_slug.vue
+++ front/pages/chat/_slug.vue
@@ -1,191 +1,170 @@
 <template>
-  <v-container
-    style="overflow-y: hidden !important"
-    class="body"
-    fluid 
-    fill-height
+<v-container
+  style="overflow-y: hidden !important"
+  class="body"
+  fluid 
+  fill-height
+>
+  <v-app-bar
+    color="#181818"
+    height="130"
+    flat
+    fixed
+    clipped-left
   >
-   <v-row style="height: 100%; margin-top: 2.5%">
-    <v-col cols="12" sm="3" class="border">
-
-
-    <v-card
-      max-width="350"
-      color="#181818"
-      flat
+    <v-btn
+      icon
+      class="neon-button"
+      style="margin-top: 80px"
+      @click.stop="channelDrawer = !channelDrawer"
+      :color="channelFocus == true ? '#9141c7' : 'black'"
+      v-on:mouseover="channelFocus = true"
+      v-on:mouseleave="channelFocus = false"
     >
-      <v-virtual-scroll
-        :items="channList"
-        height="500"
-        min-width="100"
-        item-height="60"
-      >
-        <template v-slot:default="{ item }">
-          <v-list-item :key="item" @click="redirectToChannel(item)" style="padding-bottom: 10px" align="center">
-            <v-card flat height="54" min-width="150" width="300" style="margin-bottom: -8px; border-radius: 15px" color="#181818" class="neon-button">
-            <v-card-text style="color: white">
-              <h3>{{ item }}</h3>
-            </v-card-text>
-            </v-card>
-          </v-list-item>
-          <!-- <v-divider class="divider"></v-divider> -->
-        </template>
-      </v-virtual-scroll>
-    </v-card>
-
-
-
-
-      <!-- <v-virtual-scroll
-        :bench="benched"
-        :items="channList"
-        height="500"
-        item-height="64"
-      >
-      <v-card v-for="(channel, i) in channList" :key="channList[i]"
-        tile
-        @click="redirectToChannel(channel)"
-      >
-        <v-card-text>
-          {{ channel }}
-        </v-card-text>
-        <v-divider></v-divider>
-      </v-card>
-      </v-virtual-scroll> -->
-    </v-col>
-
-
-    <v-col cols="12" sm="6" class="border">
-      <v-card
-        max-width="1000"
-        color="#181818"
-        flat
-      >
-        <v-virtual-scroll
-          :items="messagesArray"
-          height="500"
-          benched="3"
-          item-height="100"
-        >
-          <template v-slot:default="{ item }">
-            
-    <!-- <div v-for="(msg, i) in messagesArray" -->
-      <!-- :key="i" -->
-      <div
-        class="overflow-y-auto"
-        style="margin-top: 0px; position: relative; padding-right: 45px; padding-left: 45px; padding-bottom: 15px"
-      >
-      <v-img
-        :style="isYourMsg(item) ? 'float: right; margin-left: 20px !important; right: 0' : 'float: left; margin-right: 20px !important; left: 0'"
-        style="margin-top: 0px; border-radius: 30px; position: absolute; bottom: 0px;"
-        width="30"
-        :src="item.picture"
-        @click="redirectToUserProfile(item.senderNick)"
-      >
-      </v-img>
-      <v-card
-        class="bubble"
-        :class="isYourMsg(item) ? 'bubble bubble_right' : 'bubble bubble_left'"
-        :color="isYourMsg(item) ? '#1982FC' : '#ffffff'"
-        style="min-width: 70px; max-width: 400px !important; margin-top: 20px"
-      >
-        <v-card-subtitle
-          style="padding-bottom: 0px; color: white"
-          v-text="item.senderNick"
-          class="text-left"
-        >
-
-        </v-card-subtitle>
-      <v-card-text
-        style="padding-bottom: 0px; padding-right: 55px; color: white"
-        v-text="item.message"
-      >
-      </v-card-text>
-      <v-card-subtitle
-        style="padding-bottom: 5px; padding-top: 0px; color: white"
-        v-text="formateTime(item.time)"
-        class="text-right"
-      >
-      </v-card-subtitle>
-      </v-card>
-    </div>
-        </template>
-      </v-virtual-scroll>
-    </v-card>
-    </v-col>
-
-    <v-col cols="12" sm="3">
-
-          <v-card
-      max-width="350"
-      color="#181818"
-      flat
+      <v-icon> mdi-forum </v-icon>
+    </v-btn>
+    <v-spacer />
+    <h3 
+      style="color: white; margin-top: 80px"
+    >{{ $route.params.slug }}</h3>
+    <v-spacer />
+    <v-btn
+      icon
+      class="neon-button"
+      style="margin-top: 80px"
+      @click.stop="userDrawer = !userDrawer"
+      :color="userFocus == true ? '#9141c7' : 'black'"
+      v-on:mouseover="userFocus = true"
+      v-on:mouseleave="userFocus = false"
     >
-      <v-virtual-scroll
-        :items="userList"
-        height="500"
-        min-width="80"
-        item-height="64"
-      >
-        <template v-slot:default="{ item }">
-          <v-list-item :key="item" @click="redirectToUserProfile(item.nickName)">
-            <v-card flat height="60" min-width="150" width="300" style="border-radius: 15px;" color="#181818" class="neon-button">
-              <v-card-title style="padding-top: 8px; padding-bottom: 0px">
-                <v-avatar size="36">
-                  <img
-                    alt="user"
-                    :src="item.picture"
-                  >
-                </v-avatar>
-                <h4 style="color: white; margin-left: 10px">{{ item.nickName }}</h4>
-              </v-card-title>
-              <v-card-subtitle align="right">
-                <h6 style="color: white">{{ item.channelStatus }}</h6>
-              </v-card-subtitle>
-            </v-card>
-          </v-list-item>
-          <!-- <v-divider class="divider"></v-divider> -->
-        </template>
-      </v-virtual-scroll>
-          </v-card>
-      <!-- <v-card v-for="(user, i) in userList" :key="`user-${i}`"
-        tile
-        @click="redirectToUserProfile(user.nickName)"
-      >
+      <v-icon> mdi-account-group </v-icon>
+    </v-btn>
+  </v-app-bar>
+  <v-row style="height: 100%; margin-top: 2.5%">
 
-        <v-card-title>
+    <v-navigation-drawer
+      v-model="channelDrawer"
+      app
+      left
+      temporary
+      style="margin-top: 70px"
+      color="#181818"
+    >
+      <v-list dense>
+        <v-list-item
+          class="neon-button"
+          style="border-radius: 15px; margin-top: 10px"
+          v-for="(channel, i) in channList"
+          :key="channList[i]"
+          link
+          @click="redirectToChannel(channel)"
+        >
+          <v-list-item-content>
+            <h4 style="color: white; margin-left: 10px">{{ channel }}</h4>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      v-model="userDrawer"
+      app
+      right
+      temporary
+      color="#181818"
+      style="margin-top: 70px"
+    >
+      <v-list dense>
+        <v-list-item
+          class="neon-button"
+          style="border-radius: 15px; margin-top: 10px"
+          v-for="(user, i) in userList"
+          :key="`user-${i}`"
+          link
+          @click="redirectToUserProfile(user.nickName)"
+        >
+          <v-list-item-icon style="margin-right: 10px; padding-top: 4px">
           <v-avatar size="36">
             <img
               alt="user"
               :src="user.picture"
             >
           </v-avatar>
-          <p style="margin-bottom: 0px" class="ml-3">{{ user.nickName }}</p>
-        </v-card-title>
-        <v-card-subtitle style="padding-bottom: 0px; margin-bottom: 4px !important" align="right">
-          <p class="ml-12" style="margin-bottom: 0px">{{ user.channelStatus }}</p>
-        </v-card-subtitle>
-        <v-divider></v-divider>
-      </v-card> -->
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.nickName" class="text-h6" style="color: white; margin-top: 14px"></v-list-item-title>
+          <v-list-item-subtitle v-text="user.channelStatus" align="right" style="color: white; margin-top: 0px">
+          </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-col cols="12" sm="12">
+      <v-card
+        color="#181818"
+        flat
+      >
+        <div v-for="(msg, i) in messagesArray"
+          :key="i"
+          class="overflow-y-auto"
+          style="margin-top: 0px; position: relative; padding-right: 45px; padding-left: 45px; padding-bottom: 15px"
+        >
+          <v-img
+            :style="isYourMsg(msg) ? 'float: right; margin-left: 20px !important; right: 0' : 'float: left; margin-right: 20px !important; left: 0'"
+            style="margin-top: 0px; border-radius: 30px; position: absolute; bottom: 0px;"
+            width="30"
+            :src="msg.picture"
+            @click="redirectToUserProfile(msg.senderNick)"
+          >
+          </v-img>
+          <v-card
+            class="bubble"
+            :class="isYourMsg(msg) ? 'bubble bubble_right' : 'bubble bubble_left'"
+            :color="isYourMsg(msg) ? '#1982FC' : '#ffffff'"
+            style="min-width: 70px; max-width: 400px !important; margin-top: 20px"
+          >
+            <v-card-subtitle
+              style="padding-bottom: 0px; color: white"
+              v-text="msg.senderNick"
+              class="text-left"
+            >
+
+            </v-card-subtitle>
+          <v-card-text
+            style="padding-bottom: 0px; padding-right: 55px; color: white"
+            v-text="msg.message"
+          >
+          </v-card-text>
+          <v-card-subtitle
+            style="padding-bottom: 5px; padding-top: 0px; color: white"
+            v-text="formateTime(msg.time)"
+            class="text-right"
+          >
+          </v-card-subtitle>
+          </v-card>
+        </div>
+      </v-card>
     </v-col>
   </v-row>
-    <v-footer app inset color="#181818">
-      <v-text-field
-        style="margin-top: 3%"
-        background-color="white"
-        color="blue"
-        v-model="message"
-        filled
-        clear-icon="mdi-close-circle"
-        clearable
-        label="Message"
-        elevation="2"
-        @keypress.enter="sendMessage"
-      >
-        <v-icon slot="append-outer" color="blue"> mdi-send </v-icon>
-      </v-text-field>
-    </v-footer>
-  </v-container>
+  
+  <v-footer app inset color="#181818">
+    <v-text-field
+      style="margin-top: 3%"
+      background-color="white"
+      color="blue"
+      v-model="message"
+      filled
+      clear-icon="mdi-close-circle"
+      clearable
+      label="Message"
+      elevation="2"
+      @keypress.enter="sendMessage"
+    >
+      <v-icon slot="append-outer" color="blue"> mdi-send </v-icon>
+    </v-text-field>
+  </v-footer>
+</v-container>
 </template>
 
 <script lang='ts'>
@@ -203,12 +182,16 @@ export default Vue.extend({
   data() {
     return {
       message: '',
-      messagesArray: new Array<Messages>(),
+      messagesArray: new Array<String>(),
       usersNick: new Map(),
       me: new User(),
       nbMsg: -1,
       channList: [],
       userList: new Array<ChannelUser>(),
+      userDrawer: false,
+      channelDrawer: false,
+      userFocus: false,
+      channelFocus: false,
     }
   },
 
@@ -243,7 +226,10 @@ export default Vue.extend({
     this.channList = myChannelRet.data
     var userListRet = await this.$axios.get('/api/chat/' + this.$route.params.slug + '/users')
     this.userList = userListRet.data
+    console.log(userListRet)
     console.log(this.userList)
+    console.log(myChannelRet)
+    console.log(this.channList)
   },
 
   methods: {
