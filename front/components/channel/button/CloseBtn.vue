7b41cb057f6025f646a21f00c5096257788593ed
lmoulin <lucas.moulin78114@gmail.com>
2021-12-19 18:27:27 +0100

    add close btn component

---
+++ front/components/channel/button/CloseBtn.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    icon
+    class="neon-button"
+    :color="btnFocus == true ? '#9141c7' : 'black'"
+    @click.stop="btnClicked()"
+    v-on:mouseover="btnFocus = true"
+    v-on:mouseleave="btnFocus = false"
+  >
+    <v-icon> mdi-close </v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component } from "nuxt-property-decorator";
+import Vue from 'vue'
+
+@Component
+export default class CloseBtn extends Vue {
+  
+  btnFocus: boolean = false
+
+  btnClicked() {
+    this.$emit('click')
+  }
+}
+</script>
+
+<style>
+@import '../../../assets/main_page.scss';
+</style>
