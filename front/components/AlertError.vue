cf2149eacf71ee1a7095a68ea5e96d2c0dc7d331
lmoulin <lucas.moulin78114@gmail.com>
2021-12-19 04:18:56 +0100

    add alert component

---
+++ front/components/AlertError.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-alert
+    v-model="alertCode"
+    outlined
+    :type=alertType
+    text
+    dismissible
+    style="position: absolute; right: 0px; top: 30px; z-index: 12; width: 100%"
+    align="center"
+  >
+    {{ alertText }}
+    <slot></slot>
+  </v-alert>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  middleware: 'login',
+  name: "AlertError",
+
+  data() {
+    return {
+      alertCode: false,
+      alertType: 'error',
+      alertText: '',
+    }
+  },
+
+  methods: {
+    
+    activeAlert(alert: string) {
+      this.alertType = 'error'
+      this.alertText = alert
+      this.alertCode = true
+      setTimeout(() => {
+        this.alertCode = false
+      }, 2000)
+      // this.$emit("alertEnd")
+    },
+  }
+})
+</script>
