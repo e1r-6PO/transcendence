b18daa833763a5b8b4f2e294745d0b7bfb93184d
lmoulin <lucas.moulin78114@gmail.com>
2021-12-19 17:21:53 +0100

    add userList component

---
+++ front/components/channel/ChannelUserList.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-list dense>
+    <slot> </slot>
+    <v-list-item
+      class="neon-button"
+      style="border-radius: 15px; margin-top: 10px"
+      v-for="(user, i) in userList"
+      :key="`user-${i}`"
+      link
+      @click="redirectToUserProfile(user.nickName)"
+    >
+      <v-list-item-icon style="margin-right: 10px; padding-top: 4px">
+      <v-avatar size="36">
+        <img
+          alt="user"
+          :src="user.picture"
+        >
+      </v-avatar>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="user.nickName" class="text-h6" style="color: white; margin-top: 14px"></v-list-item-title>
+      <v-list-item-subtitle v-text="user.channelStatus" align="right" style="color: white; margin-top: 0px">
+      </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script lang="ts">
+import { Component } from 'nuxt-property-decorator';
+import Vue from 'vue'
+import { ChannelUser } from '../../assets/ChannelUser';
+
+@Component
+export default class ChannelUserList extends Vue {
+  
+
+  userList: Array<ChannelUser> = []
+
+  async mounted() {
+    var userListRet = await this.$axios.get('/api/chat/' + this.$route.params.slug + '/users')
+    .catch(function(error) {
+      return error.response
+    })
+    if (userListRet.status == 403)
+      this.$router.push('/chat')
+    else
+      this.userList = userListRet.data
+  }
+
+  redirectToUserProfile(userNick: string) {
+    this.$router.push("/users/" + userNick)
+  }
+}
+</script>
