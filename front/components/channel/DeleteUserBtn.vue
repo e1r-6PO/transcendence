0e14fc6d71e8c7852eb817039850c2eda88de253
lmoulin <lucas.moulin78114@gmail.com>
2021-12-22 16:27:59 +0100

    add DeleteBtn component to kick user in channel

---
+++ front/components/channel/DeleteUserBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <CloseBtn
+    v-on:click="deleteUser()"
+    content="mdi-account-minus"
+  />
+</template>
+      
+<script lang="ts">
+import { Component, Prop } from 'nuxt-property-decorator';
+import Vue from 'vue'
+
+@Component
+export default class DeleteUserBtn extends Vue{
+  
+  @Prop({ type: String, default: ""})
+  userName: string
+
+  async deleteUser() {
+    const ret = await this.$axios.delete('/api/chat/' + this.$route.params.slug + '/deleteUser?userName=' + this.userName)
+      .catch(function(error) {
+        return error.response
+    })
+    if (ret.status == 403)
+      this.activeAlert(ret.data.message)
+    else if (ret.status == 404)
+      this.$router.push('/chat?error=' + ret.data.message)
+    else
+      this.$emit('refreshUser')
+  }
+
+  activeAlert(error: string)
+  {
+    this.$emit('error', error)
+  }
+}
+</script>
