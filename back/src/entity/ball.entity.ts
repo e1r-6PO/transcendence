80d5129114948d55907a9a8eedfc944c40ea70e5
avan-pra <avan-pra@users.noreply.github.com>
2021-12-23 16:46:40 +0100

    add ball entity

---
+++ back/src/entity/ball.entity.ts
@@ -0,0 +1,18 @@
+export class Ball {
+    x: number
+    y: number
+    velocity: number
+
+    // 1400 * 1000
+
+    constructor()
+    {
+        this.x = 0.5,
+        this.y = 0.5,
+        this.velocity = 1
+    }
+
+    tick() {
+
+    }
+}
