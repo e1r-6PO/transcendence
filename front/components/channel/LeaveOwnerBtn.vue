c80a4fc2f4d64225ac5407dd8056cde455b03635
lmoulin <lucas.moulin78114@gmail.com>
2021-12-31 15:38:11 +0100

    add LeaveOwner to Component

---
+++ front/components/channel/LeaveOwnerBtn.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    max-width="600px"
+  >
+    <template v-slot:activator="{ }">
+      <BasicBtn
+        v-on:click="dialog = true"
+        content="Leave"
+        :isText="true"
+        neonColor="red"
+      />
+    </template>
+    <v-card style="background-color: #181818">
+      <v-card-title class="white--text">
+        <p class="mt-4 text-h5">Choose the new Owner of the channel</p>
+        <v-spacer />
+        <BasicBtn v-on:click="dialog = false" content="mdi-close" />
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-text-field
+            v-model="userName"
+            placeholder="User name"
+            class="mt-3 custom-select-color custom-placeholder-color custom-input-color neonText"
+            color="blue"
+            hide-details
+            filled
+            dense
+            rounded
+          ></v-text-field>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <BasicBtn
+          @click="giveOwner()"
+          :isText="true"
+          :disable="userName == ''"
+          content="Leave"
+          neonColor="red"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>  
+</template>
+
+<script lang="ts">
+import { Component } from 'nuxt-property-decorator'
+import Vue from 'vue'
+import socket_chat from '../../plugins/chat.io'
+
+@Component
+export default class LeaveOwnerBtn extends Vue {
+ 
+ dialog: boolean = false
+ userName: string = ""
+
+ async giveOwner() {
+   var ret = await this.$axios.patch('/api/chat/' + this.$route.params.slug + '/giveOwner?userName=' + this.userName)
+    .catch(function(error) {
+      return error.response
+    })
+    console.log(ret)
+  if (ret.status == 404)
+    this.$router.push('/chat?error=' + ret.data.message)
+  else if (ret.status == 403)
+    this.activeAlert(ret.data.message)
+  else
+  {
+    this.$router.push('/chat')
+    socket_chat.emit('newOwner', this.$route.params.slug, this.userName)
+  }
+ }
+
+  activeAlert(error: string)
+  {
+    this.$emit('error', error)
+  }
+
+}
+</script>
+
+<style>
+</style>
