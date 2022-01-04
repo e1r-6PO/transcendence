cb67b43d53c7a91368709215a38cd7a3412a2c3e
EudaldV98 <janeudald98@gmail.com>
2022-01-04 19:35:12 +0100

    Player paddle in progress. Done drawing, movement (input) and collisions with map

---
+++ front/assets/Classes-ts/Player.ts
@@ -0,0 +1,32 @@
+class Player {
+    constructor (x: number, y: number){
+			this.x = x;
+			this.y = y;
+			this.width = 15;
+			this.height = 60;
+			this.speed = 20;
+    }
+
+		moveUp(){
+			let res = this.y - this.speed;
+			if (res < 0)
+				this.y = 0;
+			else
+				this.y -= this.speed;
+		}
+	
+		moveDown(){
+			let res = this.y + this.speed;
+			if (res + this.height > 600)
+				this.y = 600 - this.height;
+			else
+				this.y += this.speed;
+		}
+
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		speed: number;
+}
+export { Player }
