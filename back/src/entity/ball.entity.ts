1088ce9ede78780c3a335a61188ca2add9001d38
EudaldV98 <janeudald98@gmail.com>
2022-01-08 19:18:56 +0100

    New ball rework herits from Rect

--- back/src/entity/ball.entity.ts
+++ back/src/entity/ball.entity.ts
@@ -1,29 +1,33 @@
+import { Rect, Vect } from "./game_utils"
+import { Paddle } from "./paddle.entity"
+
 enum directions {
 	stable = 0,
 	positive = 1,
 	negative = -1,
 }
 
-export class Ball {
+export class Ball extends Rect {
 	canvas_x: number
 	canvas_y: number
-	x: number
-	y: number
+	// x: number
+	// y: number
 	xd: number
 	yd: number
-	ball_size: number
-	velocity: number
+	// ball_size: number
+	speed: Vect
 
 	constructor()
 	{
+		super(10, 10)
 		this.canvas_x = 840 // map width
 		this.canvas_y = 600 // map height
-		this.x = this.canvas_x / 2, // middle of the map x
-		this.y = this.canvas_y / 2, // middle of the map y
+		this.pos.x = this.canvas_x / 2, // middle of the map x
+		this.pos.y = this.canvas_y / 2, // middle of the map y
 		this.xd = Math.floor(Math.random() * 2) == 0 ? directions.negative : directions.positive // random x directions
 		this.yd = Math.floor(Math.random() * 2) == 0 ? directions.negative : directions.positive // random y directions
-		this.ball_size = 18 / 2 // size in pixel in front, useful for the ball not to go in the edge in front
-		this.velocity = 1 // must be positive value, distance traveled per tick
+		// this.ball_size = 18 / 2 // size in pixel in front, useful for the ball not to go in the edge in front
+		this.speed = new Vect() // must be positive value, distance traveled per tick
 	}
 
 	// need to take paddle position as args
@@ -32,33 +36,58 @@ export class Ball {
 			// let paddle0y = this.canvas_y / 2
 			// const paddle1x = this.canvas_x - 40
 			// let paddle1y = this.canvas_y / 2
-
-			this.x += this.velocity * this.xd
-			this.y += this.velocity * this.yd
-			if (this.x < 0 + this.ball_size) {
-					return 0
-					// player 0 lost
-					this.xd = directions.positive
-					this.x = 0 + this.ball_size
-			}
-			if (this.y < 0 + this.ball_size) {
-					this.yd = directions.positive
-					this.y = 0 + this.ball_size
-			}
-			if (this.x > this.canvas_x - this.ball_size) {
-					return 1
-					// player 1 lost
-					this.xd = directions.negative
-					this.x = this.canvas_x - this.ball_size
-			}
-			if (this.y > this.canvas_y - this.ball_size) {
-					this.yd = directions.negative
-					this.y = this.canvas_y - this.ball_size
+			if (this.speed.x === 0 && this.speed.y === 0) {
+				this.speed.x = 10 * (Math.random() > .5 ? 1 : -1);
+				this.speed.y = 10 * (Math.random() * 2 - 1);
+				this.speed.len = 12;
 			}
 
-			//collision paddles
+			this.pos.x += this.speed.x
+			this.pos.y += this.speed.y
+
+			if (this.left < 0) {
+				// return 0
+				// player 0 lost
+				this.speed.x = -this.speed.x
+			}
+			if (this.right > this.canvas_x) {
+				// return 1
+				// player 1 lost
+				this.speed.x = -this.speed.x
+			}
+
+			if (this.top < 0) {
+				this.speed.y = -this.speed.y
+			}
+			if (this.bottom > this.canvas_y) {
+				this.speed.y = -this.speed.y
+			}
+			// if (this.speed.y < 0 && this.top < 0 ||
+			// 	this.speed.y > 0 && this.bottom > this.canvas_y) {
+			// 	this.speed.y = -this.speed.y;
+			// }
+
+			//collision paddle6 
 			return -1
 	}
 
-	//fnct coll_paddles(player)
+	checkPaddleLeft(p: Paddle){
+		// console.log('LEFT: Before Collision: ' + this.x + this.xd)
+		if (p.right > this.left && p.left < this.right && 
+			p.top < this.bottom && p.bottom > this.top)
+		{
+			this.speed.x *= -1
+		}
+		// console.log('LEFT After Collision: ' + this.x + this.xd)
+	}
+
+	checkPaddleRight(p: Paddle){
+		// console.log('RIGTHT Before Collision: ' + this.x + this.xd)
+		if (p.left < this.right && p.right > this.left &&
+			p.top < this.bottom && p.bottom > this.top)
+		{
+			this.speed.x *= -1
+		}
+		// console.log('RIGHT After Collision: ' + this.x + this.xd)
+	}
 }
