4e55cc27a1a9808da9eb4011813b0c108d687b99
EudaldV98 <janeudald98@gmail.com>
2022-01-08 19:21:10 +0100

    Added size

--- front/assets/Classes-ts/Ball.ts
+++ front/assets/Classes-ts/Ball.ts
@@ -1,10 +1,14 @@
 class Ball {
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, w: number, h: number) {
         this.x = x
         this.y = y
+        this.width = w
+        this.height = h
     }
 
     x: number
     y: number
+    width: number
+    height: number
 }
 export { Ball }
