f03b70e5b0323c7c8e007c83d67ec9a7cfbf3586
EudaldV98 <janeudald98@gmail.com>
2022-01-08 19:21:37 +0100

    Removed unnecessary stuff

--- front/assets/Classes-ts/Paddle.ts
+++ front/assets/Classes-ts/Paddle.ts
@@ -2,31 +2,13 @@ class Paddle {
     constructor (){
 			this.x = 0;
 			this.y = 0;
-			this.width = 15;
-			this.height = 60;
-			this.speed = 20;
+			this.width = 0;
+			this.height = 0;
     }
 
-	moveUp(){
-		let res = this.y - this.speed;
-		if (res < 0)
-			this.y = 0;
-		else
-			this.y -= this.speed;
-	}
-	
-	moveDown(){
-		let res = this.y + this.speed;
-		if (res + this.height > 600)
-			this.y = 600 - this.height;
-		else
-			this.y += this.speed;
-	}
-
-		x: number;
-		y: number;
-		width: number;
-		height: number;
-		speed: number;
+	x: number;
+	y: number;
+	width: number;
+	height: number;
 }
 export { Paddle }
