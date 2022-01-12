1d02d342f9700a10f760710eccb7fbeeb20f0725
lmoulin <lucas.moulin78114@gmail.com>
2022-01-12 16:00:39 +0100

    add component ProfilePicture

---
+++ front/components/profile/ProfilePicture.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-btn
+    @click="sendClick()"
+    :width="size"
+    :disabled="disable"
+    icon
+    class="ml-3"
+    style="border-radius: 100%"
+  >
+    <img
+      style="border-radius: 100% !important;"
+      :style="'width: ' + size + 'px;'"
+      :src="src"
+      :class="getClasses()"
+    />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'nuxt-property-decorator'
+import Vue from 'vue'
+
+@Component
+export default class ProfilePicture extends Vue {
+  
+  @Prop({ default: "40" })
+  size!: Number | String
+
+  @Prop({ type: String, default: "" })
+  src!: String
+
+  @Prop({ type: Boolean, default: null})
+  isActive!: Boolean
+
+  @Prop({ type: Boolean, default: false})
+  disable!: Boolean
+
+  @Prop({ type: String, default: "" })
+  neonColor!: String
+
+  getClasses(): string {
+    var classes = ""
+    if (this.neonColor != "")
+      classes = "profile-picture-" + this.neonColor
+    else if (this.isActive != null)
+      classes = this.isActive == true ? 'profile-picture-green' : 'profile-picture-red'
+    return classes
+  }
+
+  sendClick() {
+    this.$emit("click")
+  }
+}
+</script>
+
+<style>
+@import '../../assets/Classes-scss/profilePicture_colors.scss';
+</style>
