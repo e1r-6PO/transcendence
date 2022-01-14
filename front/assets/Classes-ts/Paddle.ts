047a232f59357d2c641032cb185c8945161ef821
EudaldV98 <janeudald98@gmail.com>
2022-01-14 22:36:48 +0100

    Now Draw in paddle and color updated too

--- front/assets/Classes-ts/Paddle.ts
+++ front/assets/Classes-ts/Paddle.ts
@@ -1,16 +1,35 @@
 class Paddle {
-    constructor (){
-			this.x = 0;
-			this.y = 0;
-			this.width = 0;
-			this.height = 0;
-			this.color = "";
-    }
+	x: number
+	y: number
+	width: number
+	height: number
+	color: string
+	shadowColor: string
 
-	x: number;
-	y: number;
-	width: number;
-	height: number;
-	color: string;
+    constructor (){
+			this.x = 0
+			this.y = 0
+			this.width = 0
+			this.height = 0
+			this.color = ""
+			this.shadowColor = ""
+    }
+	
+	draw(maptest: CanvasRenderingContext2D){
+		maptest.beginPath()
+		maptest.fillStyle = this.color
+		if (this.color == 'purple')
+			this.shadowColor = 'rebeccapurple'
+		else if (this.color == 'yellow')
+			this.shadowColor = 'goldenrod'
+		else if (this.color == 'pink')
+			this.shadowColor = 'darkviolet'
+		else
+			this.shadowColor = 'dark' + this.color
+		maptest.shadowColor = this.shadowColor
+		maptest.shadowBlur = 8
+		maptest.fillRect(this.x, this.y, this.width, this.height)  
+		maptest.closePath()
+	}
 }
 export { Paddle }
