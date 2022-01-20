0ba00208be711c305e72cbe2c89008a1e81b2c1f
EudaldV98 <janeudald98@gmail.com>
2022-01-20 12:56:46 +0100

    Added new font for content in pages (Orbitron) can be changed easily

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -34,6 +34,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~layouts/global.css",
+    "~layouts/Orbitron.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
