a8d08b7ce1f0a0df07a5435fc266e1c96aa3a677
lmoulin <lucas.moulin78114@gmail.com>
2022-01-21 05:21:56 +0100

    add achievement component in front

---
+++ front/components/achievements/AchievementCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="white--text ml-2 mr-2 mt-4 card" width="200" align="center">
+    <v-list-item>
+    <v-list-item-content>
+    <v-list-item-title v-text="achievement.title"
+      align="start"
+      class="white--text text-h6 pl-1 pb-2"
+      :class="value == '100.0' ? 'neonText-light-blue': 'neonText'"
+    />
+    <v-list-item-subtitle v-text="achievement.action" class="white--text" align="start" />
+    </v-list-item-content>
+    <v-icon
+      color="white"
+      :class="value == '100.0' ? 'neonText-light-blue': 'neonText'"
+    >
+      {{ achievement.icon }}
+    </v-icon>
+    </v-list-item>
+    <v-card-actions class="justify-center pt-0 pb-4">
+      <CircleProgress :value="value" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'nuxt-property-decorator'
+import Vue from 'vue'
+import { Achievements } from '../../assets/Classes-ts/Achievements'
+
+@Component({})
+export default class AchievementCard extends Vue{
+  
+  @Prop({ type: Object, default: new Achievements()})
+  achievement!: Achievements
+
+  @Prop({ default: 0 })
+  value!: Number | String
+
+}
+</script>
---
+++ front/components/achievements/CircleProgress.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-progress-circular
+      :rotate="-90"
+      :size="75"
+      :width="10"
+      :color="value == '100.0' ? '#a5fafa': '#fc6500'"
+      :class="value == '100.0' ? 'success-circular': 'test'"
+      :value="value"
+    >
+      {{ value }}%
+    </v-progress-circular>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'nuxt-property-decorator'
+import Vue from 'vue'
+import { Achievements } from '../../assets/Classes-ts/Achievements'
+
+@Component({})
+export default class AchievementCard extends Vue{
+
+  @Prop({default: 0})
+  value!: Number | String
+
+}
+</script>
