656e81d6dee46ccd7f244423bcaf9c2817069d4f
lmoulin <lucas.moulin78114@gmail.com>
2022-01-21 05:19:26 +0100

    add achievement path for component

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: ['~/components/', '~/components/channel/', '~/components/channel/button', '~/components/profile'],
+  components: ['~/components/', '~/components/channel/', '~/components/channel/button', '~/components/profile', '~/components/achievements'],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
