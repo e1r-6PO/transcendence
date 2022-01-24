6955b69569705bf21e1cab25f33751fd388f6ffc
avan-pra <avan-pra@users.noreply.github.com>
2022-01-24 20:15:51 +0100

    add stats about online players

---
+++ front/components/home/numberConnectedUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="color: white">
+    {{ amount }}
+    <v-icon
+      color="red"
+    >
+      mdi-account
+    </v-icon>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+import Vue from 'vue'
+
+@Component
+export default class numberConnectedUser extends Vue {
+
+  @Prop({type: Number})
+  amount!: number
+}
+</script>
+
+<style>
+
+</style>
