e853dd7c14b9a6667f352190319b8a137e1f9b9f
avan-pra <avan-pra@users.noreply.github.com>
2022-01-29 04:24:30 +0100

    allow nuxt to play sound

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -121,5 +121,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      })
+    },
   }
 }
