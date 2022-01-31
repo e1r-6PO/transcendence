c9a644e0dd8966c4f9486e5b5b06998c055a10bf
lmoulin <lucas.moulin78114@gmail.com>
2022-01-31 17:56:57 +0100

    add OG color paddle

--- back/src/controller/achievements.controller.ts
+++ back/src/controller/achievements.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from 'express';
+import { Achievements } from 'src/entity/achievements.entity';
+import { AchievementsList } from 'src/entity/achievementsList.entity';
 import { TwoFaGuard, ValidTokenGuard } from 'src/guards/account.guards';
 import { AchievementsService } from 'src/service/achievements.service';
+import { Repository } from 'typeorm';
 
 @Controller('api/achievements')
 @UseGuards(ValidTokenGuard, TwoFaGuard)
@@ -9,11 +13,19 @@ export class AchievementsController {
 
   constructor(
     private achievementsService: AchievementsService,
+    @InjectRepository(Achievements)
+    private readonly achievementsRepository: Repository<Achievements>,
   )
   {}
 
   @Get()
-  async getAllAchievements() {
+  async getAllAchievements( @Req() req: Request) {
     return this.achievementsService.getAllAchievement()
+  }
+
+  @Get(':title')
+  async getOne(@Param('title') title, @Req() req: Request) {
+
+    return this.achievementsService.getOneByTitle(title)
   }
 }
